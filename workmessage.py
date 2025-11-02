91d921b13da52b836d9ab67c840fce3c9fa05ffe
annenxu <anne.nxumalo@icloud.com>
2025-11-03 01:18:23 +0200

    Fix spacing in motivational messages
    Removed extra spaces from message strings.

--- workmessage.py
+++ workmessage.py
@@ -9,8 +9,8 @@ st.markdown("I'm so proud of you and so excited that we're buying Robben Island 
 if st.button("Click here, daddy Doerr 🙂‍↕️"):
     messages = [
         "Jokes aside, I am so so so proud of you Niki-poo."
-        "Good luck on your very first day as a full time office zaddy! I still don't know exactly what you do but those Teams emojis and Excel functions don't know what's coming today."
-        "You may not want to hear this but know that I'm praying for your joy, success and progression in this role. I believe with my whole heart He'll protect you and strengthen you."
+        "Good luck on your very first day as a full time office zaddy! I still don't know exactly what you do but those Teams emojis and Excel functions don't know what's coming today. "
+        "You may not want to hear this but know that I'm praying for your joy, success and progression in this role. I believe with my whole heart He'll protect you and strengthen you. "
         "Curtis and I are thinking of you today, and love you a lot! We don't care that you're the office weirdo, we think you're cool ❤️❤️"
     ]
     message = random.choice(messages)
